aac2cd908bca963555de22562bdeae493a871f09
LeeSeokmin <dltjrals13@naver.com>
2025-02-26 14:43:15 +0900

    mdx 연동 완료

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import nextMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   images: {
@@ -10,6 +11,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
