391221d767c7e5bbc208847b8d29f4a057fd286f
LeeSeokmin <dltjrals13@naver.com>
2025-02-27 20:07:30 +0900

    툴팁 margin-top 삭제

--- src/app/components/tooltip.tsx
+++ src/app/components/tooltip.tsx
@@ -2,7 +2,7 @@ export default function Tooltip({ title, bgColor, textColor }: { title: string, 
   return (
     <div 
       className={`absolute top-full left-1/2 transform -translate-x-1/2 
-      mt-2 w-max px-2 py-1 text-sm text-${textColor} bg-${bgColor} rounded
+      w-max px-2 py-1 text-sm text-${textColor} bg-${bgColor} rounded
       shadow-lg opacity-0 group-hover:opacity-100`}
     >
         {title}
