1ccd921b65801273dc66b8c37e9e438d200ae4da
LeeSeokmin <dltjrals13@naver.com>
2025-02-28 21:57:52 +0900

    코딩하지 마세요: 내가 블로그를 만든 이유 포스팅

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,11 @@ import type { NextConfig } from "next";
 import nextMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
-  content: [
-    './app/**/*.{js, ts, jsx, tsx, mdx}',
-    './pages/**/*.{js, ts, jsx, tsx, mdx}',
-    './components/**/*.{js, ts, jsx, tsx, mdx}',
-  ],
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
 };
 
 const withMDX = nextMDX({
-  extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
