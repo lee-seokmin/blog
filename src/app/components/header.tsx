706be047d55f16c99346354b374f04628fb1fa2e
LeeSeokmin <dltjrals13@naver.com>
2025-02-28 10:25:49 +0900

    헤더 액션 수정

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,14 +1,54 @@
 "use client";
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { useTheme } from '@/app/context/ThemeContext';
 
 export default function Header() {
+  const headerRef = useRef<HTMLElement>(null);
+  const [headerHeight, setHeaderHeight] = useState(0);
   const { isDark, toggleDarkMode } = useTheme();
   const [isRotating, setIsRotating] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setIsVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    const header = headerRef.current;
+    if (!header) return;
+
+    // Set initial height
+    setHeaderHeight(header.offsetHeight);
+
+    // Create ResizeObserver to handle dynamic height changes
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        setHeaderHeight((entry.target as HTMLElement).offsetHeight);
+      }
+    });
+
+    resizeObserver.observe(header);
+
+    // Update document padding
+    document.body.style.paddingTop = `${headerHeight}px`;
+
+    return () => {
+      resizeObserver.disconnect();
+      document.body.style.paddingTop = '0px';
+    };
+  }, [headerHeight]);
 
   const handleToggle = () => {
     setIsRotating(true);
@@ -17,7 +57,12 @@ export default function Header() {
   };
 
   return (
-    <header className="flex bg-background text-foreground p-2 md:p-5 justify-between items-center sticky top-0 z-50">
+    <header
+      ref={headerRef}
+      className={`fixed w-full top-0 z-50 transition-transform duration-300 bg-background text-foreground p-2 md:p-5 flex justify-between items-center ${
+        isVisible ? 'translate-y-0' : '-translate-y-full'
+      }`}
+    >
       <Link href="/" className="flex">
         <p className="font-bold text-2xl italic blog-shine">
           Dev Blog.
