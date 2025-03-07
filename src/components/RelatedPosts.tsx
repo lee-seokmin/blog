2b302d424ec86ffc38e39022204c271dafb8c70f
LeeSeokmin <dltjrals13@naver.com>
2025-03-07 09:51:23 +0900

    RelatedPosts 포스트 갯수 수정

--- src/components/RelatedPosts.tsx
+++ src/components/RelatedPosts.tsx
@@ -6,7 +6,7 @@ import type { MdxContent } from '@/types/MdxContent';
 export default function RelatedPosts({ related_posts }: { related_posts: MdxContent[] }) {
   return (
     <div className="mt-16">
-      <p className="text-2xl font-bold mb-4">관련된 포스트가 {RelatedPosts.length}개 있어요.</p>
+      <p className="text-2xl font-bold mb-4">관련된 포스트가 {related_posts.length}개 있어요.</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         {related_posts
           .slice(0, 4)
